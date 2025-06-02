881d58ba05b83fbe57a89db6d267a4001424b1e6
42 <33488710+FortyTwoFortyTwo@users.noreply.github.com>
2025-06-02 13:01:02 +0100

    Don't use npm

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 // Zod - NPM - This works best with the AI SDK. Other's cause versioning issues.
-export { z } from 'npm:zod';
+export { z } from 'zod';
 
 // MongoDB - NPM
-export { ObjectId } from 'npm:mongodb@6.1.0';
+export { ObjectId } from 'mongodb@6.1.0';
